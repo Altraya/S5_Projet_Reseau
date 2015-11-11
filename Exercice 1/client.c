d262d7d4d1349a1b273b173a95cd749e944d12ec
Altraya <karakayn@darluok-server.com>
2015-11-11 16:24:53 +0100

    Add first Exercice (Server bug when you try to send a file with more than 1024 caracters)
    Add the same files in Exercice 2 to continue.

---
+++ Exercice 1/client.c
@@ -0,0 +1,74 @@
+
+/*======================================================
+			client.c
+	Transfert de fichiers à sens unique
+./client <fichier_a_envoyer> <adr_IP_dist> <port_dist>
+./client coucou.txt 192.168.132.128 5000
+ ======================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>          
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+#define BUFFER_LENGTH 1024
+
+
+int main(int argc, char **argv)
+{
+	int input_fd;
+	int nbchar;
+	if (argc < 4){
+		fprintf(stderr, "Usage: %s <fichier_a_envoyer> <adr_IP_dist> <port_dist>\n", argv[0]);
+		exit(1);
+	};
+	char* nomFichier = argv[1];
+	char* adresseIp = argv[2];
+	int port = atoi(argv[3]);
+	{
+		struct sockaddr_in adr;
+		adr.sin_family = AF_INET;
+		inet_pton(AF_INET,adresseIp,&(adr.sin_addr));
+		adr.sin_port = htons(port); 
+		int fd;
+		fd=socket(AF_INET,SOCK_DGRAM,0);
+		if(fd==-1)
+		{
+			perror("socket");
+			exit(1);
+		}
+		else
+		{
+			// ouvrir le fichier en lecture
+			input_fd = open(nomFichier, O_RDONLY);
+			if(input_fd<0)
+			{	
+				perror("open input"); 
+				exit(1); 		
+			}
+
+			char buffer[BUFFER_LENGTH];
+
+			int finished=0;
+			while(!finished)
+			{
+				int nb_lu = read(input_fd, buffer, BUFFER_LENGTH);
+				if(nb_lu < BUFFER_LENGTH)
+					finished = 1;
+				socklen_t a =sizeof(adr); 
+				nbchar=sendto(fd, buffer,nb_lu,0, (struct sockaddr*)&adr,a);
+				printf("Nombre de caractère envoyé : %d\n",nbchar);
+			}
+		}
+		close(input_fd);
+		return 0;
+	}
+}
+
---
+++ Exercice 1/serveur.c
@@ -0,0 +1,77 @@
+
+/*======================================================
+			serveur.c
+	Transfert de fichiers à sens unique
+./serveur <fichier_recu> <port_locale>
+./serveur coucou.txt 5000
+ ======================================================*/
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>          
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#define BUFFER_LENGTH 1024
+
+int main(int argc, char **argv)
+{
+	if (argc < 3){
+		fprintf(stderr, "Usage: %s <fichier_recu> <port_local>\n", argv[0]);
+		exit(1);
+	};
+	char* nomFichier = argv[1];
+	printf("Nom fichier : %s\n", nomFichier);
+	int port = atoi(argv[2]);
+	printf("Port : %d\n", port);
+	int output_fd;
+	struct sockaddr_in adr;
+	adr.sin_family = AF_INET;
+	adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	adr.sin_port = htons(port); 
+	int fd;
+	fd=socket(AF_INET,SOCK_DGRAM,0);
+	
+	if(fd==-1)
+	{
+		perror("socket");
+		exit(1);
+	}
+	else
+	{
+		if(bind(fd,(struct sockaddr*)&adr,sizeof(adr))==-1)
+		{
+			perror("bind");
+			exit(2);
+		}
+		else
+		{		
+			// ouvrir le fichier en ecriture
+			output_fd = open(nomFichier, O_CREAT | O_EXCL | O_WRONLY, S_IRUSR | S_IWUSR);
+			if(output_fd<0)
+			{	
+				perror("open output"); 
+				exit(1); 		
+			}
+
+			// copie :
+			char buffer[BUFFER_LENGTH];
+			int finished=0;
+			printf("Avant boucle\n");
+			while(!finished)
+			{
+				socklen_t a =sizeof(adr); 
+				int nb_lu = recvfrom(fd,buffer,1024,0,(struct sockaddr*)&adr,&a);
+				printf("Recu : %d octets\n", nb_lu);
+				if(nb_lu < BUFFER_LENGTH)
+					finished = 1;
+				write(output_fd, buffer, nb_lu);
+			}
+		}
+		close(output_fd);
+		return 0;
+	}
+}
