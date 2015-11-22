3a03702597c623937aed77c8bdae25cdef54c632
Altraya <karakayn@darluok-server.com>
2015-11-22 21:07:36 +0100

    Add the complete exercice 1 to the exercice 2 folder, to have a base for working.

--- Exercice 2/client.c
+++ Exercice 2/client.c
@@ -32,43 +32,42 @@ int main(int argc, char **argv)
 	char* nomFichier = argv[1];
 	char* adresseIp = argv[2];
 	int port = atoi(argv[3]);
+	
+	struct sockaddr_in adr;
+	adr.sin_family = AF_INET;
+	inet_pton(AF_INET,adresseIp,&(adr.sin_addr));
+	adr.sin_port = htons(port); 
+	int fd;
+	fd=socket(AF_INET,SOCK_DGRAM,0);
+	if(fd==-1)
 	{
-		struct sockaddr_in adr;
-		adr.sin_family = AF_INET;
-		inet_pton(AF_INET,adresseIp,&(adr.sin_addr));
-		adr.sin_port = htons(port); 
-		int fd;
-		fd=socket(AF_INET,SOCK_DGRAM,0);
-		if(fd==-1)
-		{
-			perror("socket");
-			exit(1);
-		}
-		else
-		{
-			// ouvrir le fichier en lecture
-			input_fd = open(nomFichier, O_RDONLY);
-			if(input_fd<0)
-			{	
-				perror("open input"); 
-				exit(1); 		
-			}
-
-			char buffer[BUFFER_LENGTH];
-
-			int finished=0;
-			while(!finished)
-			{
-				int nb_lu = read(input_fd, buffer, BUFFER_LENGTH);
-				if(nb_lu < BUFFER_LENGTH)
-					finished = 1;
-				socklen_t a =sizeof(adr); 
-				nbchar=sendto(fd, buffer,nb_lu,0, (struct sockaddr*)&adr,a);
-				printf("Nombre de caractère envoyé : %d\n",nbchar);
-			}
-		}
-		close(input_fd);
-		return 0;
+		perror("socket");
+		exit(1);
 	}
+	else
+	{
+		// ouvrir le fichier en lecture
+		input_fd = open(nomFichier, O_RDONLY);
+		if(input_fd<0)
+		{	
+			perror("open input"); 
+			exit(1); 		
+		}
+
+		char buffer[BUFFER_LENGTH];
+		int nb_lu = 0;
+		int finished=0;
+		while(!finished)
+		{
+			nb_lu = read(input_fd, buffer, BUFFER_LENGTH);
+			if(nb_lu < BUFFER_LENGTH)
+				finished = 1;
+			socklen_t a =sizeof(adr); 
+			nbchar=sendto(fd, buffer,nb_lu,0, (struct sockaddr*)&adr,a);
+			printf("Nombre de caractère envoyé : %d\n",nbchar);
+		}
+	}
+	close(input_fd);
+	return 0;
 }
 
