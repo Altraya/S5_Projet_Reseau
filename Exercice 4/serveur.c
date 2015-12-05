77a3514037cb35cb6b13236390b10b9a09334b0f
EliseAius <elise.moebs@gmail.com>
2015-12-05 22:25:02 +0100

    Update serveur.c
    Il ne fait pas le premier sendto. Je ne sais pas pourquoi.

--- Exercice 4/serveur.c
+++ Exercice 4/serveur.c
@@ -2,7 +2,7 @@
 /*======================================================
 			serveur.c
 			Bit Alterné
-./serveur <fichier_a_envoyer> <fichier_recu> <port_locale>
+./serveur <fichier_a_envoyer> <fichier_recu> <port_local>
 ./serveur envoieServ.txt recuServ.txt 5000
  ======================================================*/
 
@@ -17,20 +17,30 @@
 #include <string.h>
 #define BUFFER_LENGTH 1024
 
-typedef struct message_s 
+typedef struct message_s
 {
 	char buf[BUFFER_LENGTH];
 	short int fin;
-	int taille;
+	int taille; // en octets
 	short int bit;
-	short int ack;
+	short int ack; // vaut 1 si c'est un ack
+	short int flagEnPlus; // vaut 0 quand on fait un échange
+						  // du client au serveur. 1 sinon
+	short int connexion; //vaut 1 si c'est le datagramme de connexion
 }message;
 
 message* initMessage()
 {
 	message* m = malloc(sizeof(struct message_s));
-	m->fin=0;
+	m->fin = 0;
+	m->bit = 0;
 	return m;
+}
+
+void printMessage (message* mess, char* nomMess)
+{
+	printf( "%s :\t fin=%d\n \t bit=%d\n\tack=%d\n\tflagEnPlus=%d\n\tconnexion=%d\n\tbuf=%s\n",nomMess,mess->fin,mess->bit,mess->ack,mess->flagEnPlus,mess->connexion,mess->buf);	
+	printf("\n");
 }
 
 int main(int argc, char **argv)
@@ -74,22 +84,21 @@ int main(int argc, char **argv)
 	//Connexion : adresse du client qui nous a contacté
 	struct sockaddr_in adrDist;
 	adrDist.sin_family = AF_INET;
-	socklen_t addrDist = sizeof(adrDist);
 
 	message* buffConnexion = initMessage();
-
+	message* ack_conn = initMessage();
+	ack_conn->connexion = 1;
+	ack_conn->ack = 1;
 	int nbCharCon = 0;
-	socklen_t b = sizeof(adrDist);
+	socklen_t addrLocale = sizeof(adrLocale);
+	socklen_t addrDist = sizeof(adrDist);
 
 	printf("En attente d'une connexion \n");
-	nbCharCon = recvfrom(fd, buffConnexion, 0, 0, (struct sockaddr*)&adrDist, &b);
-	buffConnexion->ack=1;
-	printf("On a reçu un datagramme de connexion.\n");
-	sendto(fd, buffConnexion, sizeof(message), 0, (struct sockaddr*)&adrDist, addrDist);
-	printf("On a envoyé un ack. \n Connexion en cours : Le serveur a recu un datagramme de connexion\n");
-	
+	nbCharCon = recvfrom(fd, buffConnexion, sizeof(message), 0, (struct sockaddr*)&adrLocale, &addrLocale);
+	nbCharCon = sendto(fd, buffConnexion, sizeof(message), 0, (struct sockaddr*)&adrDist, addrDist);
+	printMessage(buffConnexion,"je t'envoie ack connexion");
 	printf("Ip adresse locale : %s\n", inet_ntoa(adrLocale.sin_addr));
-	printf("Port locale %d\n", ntohs(adrLocale.sin_port));
+	printf("Port local %d\n", ntohs(adrLocale.sin_port));
 
 
 	printf("Ipclient : %s \n", inet_ntoa(adrDist.sin_addr));
@@ -120,50 +129,134 @@ int main(int argc, char **argv)
 	message* messageRecu = initMessage();
 	message* messageAEnvoyer = initMessage();
 	message* msg_ack = initMessage();
+	msg_ack->ack=1;
 	int nbLuEnvoi = 0;
 	int nbLuRecoi = 0;
-	socklen_t addrLocale = sizeof(adrLocale);
-	short int ack_v = 0;
+	short int lireLaSuite = 0;
+	short int ecrireLaSuite = 0;
+	short int bit_attendu = 0;
+	short int bit_a_envoyer = 0;
+	fd_set readset_c;
+	int result_c;
+	struct timeval tv_c;
+
 	while(!(messageRecu->fin && messageAEnvoyer->fin))
 	{
-		//tant qu'il y a des messages à recevoir
+
 		if(!messageRecu->fin)
 		{
 			nbLuRecoi = recvfrom(fd, messageRecu, sizeof(message), 0,(struct sockaddr*)&adrLocale, &addrLocale);
-			//quand on reçoit un message on doit envoyer le ack.
-			printf("Le serveur a recu %d octets bit à %d \n", nbLuRecoi, messageRecu->bit);
-			msg_ack->bit = messageRecu->bit;
-			msg_ack->ack = 1;
-			//si jamais le ack se perd malencontreusement, le client va renvoyer le même message
-			//qu'il faudra ignorer par la suite.
-			printf("envoi ack bit à %d\n", msg_ack->bit);
-			sendto(fd, msg_ack, sizeof(message), 0, (struct sockaddr*)&adrDist, addrDist);
-			if(messageRecu->fin)
-				printf("Le serveur ferme la connexion l'émetteur (client) a envoyé une demande de fermeture\n");
-			//si on a bien la trame attendue on consomme les données
-			if(messageRecu->bit==ack_v)
+			printf("Le serveur a recu %d octets \n", nbLuRecoi);
+			printMessage(messageRecu,"messageRecu");
+			if(messageRecu->connexion==1)
 			{
-				write(output_fd, messageRecu->buf, messageRecu->taille);
+				printf("je t'envoie le ack de connexion.\n");
+				sendto(fd,buffConnexion,sizeof(message),0,(struct sockaddr*)&adrDist, addrDist);
 			}
-			//sinon, on s'en branle
+			else
+			{
+				if(messageRecu->fin)
+				{
+					printf("Le serveur ferme la connexion l'émetteur (client) a envoyé une demande de fermeture\n");
+				}
+				if(messageRecu->ack==1)
+				{
+					lireLaSuite=0;
+					bit_a_envoyer=(bit_a_envoyer+1)%2;
+				}
+				if(messageRecu->ack==0 && messageRecu->bit!=bit_attendu)
+				{
+					ecrireLaSuite=1;
+					//envoi ack		
+					sendto(fd,msg_ack,sizeof(message),0,(struct sockaddr*)&adrDist, addrDist);
+				}
+				else if(messageRecu->ack==0 && messageRecu->bit==bit_attendu)
+				{
+					ecrireLaSuite=0;
+					//envoi ack
+					sendto(fd,msg_ack,sizeof(message),0,(struct sockaddr*)&adrDist, addrDist);
+					bit_attendu=(bit_attendu+1)%2;
 
-			//on actualise le numéro de la prochaine trame attendue
-			(ack_v+=1)%2; 
+				}
+				if(messageRecu->ack==0 && ecrireLaSuite==0)	
+					write(output_fd, messageRecu->buf, messageRecu->taille);
+			} 
 		}
 
 		if(!messageAEnvoyer->fin)
 		{
-			nbLuEnvoi = read(input_fd, messageAEnvoyer->buf, BUFFER_LENGTH);
-			messageAEnvoyer->taille=nbLuEnvoi;
-			if(nbLuEnvoi < BUFFER_LENGTH)
+			
+			// Initialize the set
+			FD_ZERO(&readset_c);
+			FD_SET(fd, &readset_c);
+
+			// Initialize time out struct
+			tv_c.tv_sec = 0;
+			tv_c.tv_usec = 1000 * 1000;
+
+			if(lireLaSuite == 0)
 			{
-				messageAEnvoyer->fin=1;
-				printf("Le serveur envoi une demande de fermeture de connexion \n");			
+				nbLuEnvoi = read(input_fd, messageAEnvoyer->buf, BUFFER_LENGTH);
+				messageAEnvoyer->taille=nbLuEnvoi;
+				if(nbLuEnvoi < BUFFER_LENGTH)
+				{
+					messageAEnvoyer->fin=1;
+					printf("Le serveur envoi une demande de fermeture de connexion \n");			
+				}
 			}
 			
 			nbchar=sendto(fd, messageAEnvoyer, sizeof(message), 0, (struct sockaddr*)&adrDist, addrDist);
+			bit_attendu=(bit_attendu+1)%2;	
 			printf("Le serveur envoi %d octets \n", nbchar);
+			result_c = select(fd+1, &readset_c, NULL, NULL, &tv_c);
+			if(result_c <0)
+			{
+				perror("select");
+				exit(2);
+			}
+			if(FD_ISSET(fd, &readset_c))
+			{
+				nbCharCon = recvfrom(fd, messageRecu, sizeof(message), 0, (struct sockaddr*)&adrLocale, &addrLocale);
+				if(messageRecu->connexion==1)
+				{
+					printf("gros bordel de merde.\n");
+					sendto(fd,buffConnexion,sizeof(message),0,(struct sockaddr*)&adrDist, addrDist);
+				}
+				else
+				{
+					if(messageRecu->fin)
+					{
+						printf("Le serveur ferme la connexion l'émetteur (client) a envoyé une demande de fermeture\n");
+					}
+					if(messageRecu->ack==1)
+					{
+						lireLaSuite=0;
+						bit_a_envoyer=(bit_a_envoyer+1)%2;
+					}
+					if(messageRecu->ack==0 && messageRecu->bit!=bit_attendu)
+					{
+						ecrireLaSuite=1;
+						//envoi ack		
+						sendto(fd,msg_ack,sizeof(message),0,(struct sockaddr*)&adrDist, addrDist);
+					}
+					else if(messageRecu->ack==0 && messageRecu->bit==bit_attendu)
+					{
+						ecrireLaSuite=0;
+						//envoi ack
+						sendto(fd,msg_ack,sizeof(message),0,(struct sockaddr*)&adrDist, addrDist);
+						bit_attendu=(bit_attendu+1)%2;
+
+					}
+					if(messageRecu->ack==0 && ecrireLaSuite==0)	
+						write(output_fd, messageRecu->buf, messageRecu->taille);
+				} 
+			}
+			if(result_c==0)
+			{
+				lireLaSuite=1;
+			}
 		}
+		
 
 	}
 	close(input_fd);
